791565399762a3e4dfb3d612d40b52a10c2525b4
Tom Trahan <tomt@circleci.com>
2018-08-16 12:44:36 -0700

    add pg db

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 app = express()
 
@@ -19,4 +24,15 @@ app
 	  }
 	  res.send(result)
 	  })
+  .get('/db', async (req, res) => {
+    try {
+	  const client = await pool.connect()
+	  const result = await client.query('SELECT * FROM test_table');
+	  res.render('pages/db', result);
+		client.release();
+	} catch (err) {
+	  console.error(err);
+	  res.send("Error " + err);
+	}
+	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
