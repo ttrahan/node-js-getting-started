31bf34558524b2190daf28d97d98fcb6f1c0348a
Tom Trahan <tomt@circleci.com>
2018-08-20 15:19:39 -0700

    use heroku orb to deploy app to heroku

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true
+// });
 
 app = express()
 
@@ -24,29 +24,16 @@ app
 	  }
 	  res.send(result)
 	  })
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-		console.log(result)
-		res.render('pages/db', {results: result});
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+  // .get('/db', async (req, res) => {
+  //   try {
+  //     const client = await pool.connect()
+  //     const result = await client.query('SELECT * FROM test_table');
+	// 	console.log(result)
+	// 	res.render('pages/db', {results: result});
+  //     client.release();
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.send("Error " + err);
+  //   }
+  // })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-// app.get('/db', async (req, res) => {
-//   try {
-//     const client = await pool.connect()
-//     const result = await client.query('SELECT * FROM test_table');
-// 	  res.render('pages/db', {results: result}result);
-//     client.release();
-//   } catch (err) {
-//     console.error(err);
-//     res.send("Error " + err);
-//   }
-// });
